4aea8bd519fed5b2bfb80e4aa6d72e67689cf4aa
Jeims19 <padillamattos.jose@gmail.com>
2023-02-01 17:41:50 -0500

    correccion de capas

---
+++ CapaEntidades/Rol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Rol
+    {
+        public int Id_rol { get; set; }
+        public string Descripcion { get; set; }
+        public string Fecha_registro { get; }
+
+    }
+}
